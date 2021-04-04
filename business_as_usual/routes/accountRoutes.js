4949e33ee62be7dde8265c001761c9b82f8bcf95
Maxx <maxhiga.hawaii@gmail.com>
2021-04-03 17:22:47 -1000

    added comments for accountRoutes.js

--- business_as_usual/routes/accountRoutes.js
+++ business_as_usual/routes/accountRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const auth = require("../middleware/auth");
+// const auth = require("../middleware/auth");
 const { register, login } = require("../controllers/accountController");
 
+//routes for accounts add the /register for example to the url ending with
+// /api/accounts to post them
 router.post("/register", register);
 router.post("/login", login);
 
